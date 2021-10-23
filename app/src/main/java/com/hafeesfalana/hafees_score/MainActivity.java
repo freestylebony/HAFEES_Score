ef9c53f0371d64c60647369f2e81c66ac1405485
hafees <hafees.falana@gmail.com>
2021-10-23 16:23:04 +0100

    Initial commit

---
+++ app/src/main/java/com/hafeesfalana/hafees_score/MainActivity.java
@@ -0,0 +1,18 @@
+package com.hafeesfalana.hafees_score;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        System.out.println("Hello World");
+
+    }
+}
